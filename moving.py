6a1ba550c1e6ba8b3a7640953ec5fb79c837c229
Suren <ec18473@qmul.ac.uk>
2019-07-19 20:58:07 +0100

    Added feature extraction sucessfully

--- moving.py
+++ moving.py
@@ -4,9 +4,12 @@ import pyqtgraph as pg
 import numpy as np
 import pickle,sys
 from sklearn.cluster import DBSCAN
+import math
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
+import cv2 as cv2
+
 
 scannum = -1
 
@@ -46,9 +49,6 @@ def newscan(scannum):
 
 
 
-
-
-
 class MyWidget(pg.GraphicsWindow):
 
     def __init__(self, parent=None):
@@ -58,7 +58,7 @@ class MyWidget(pg.GraphicsWindow):
         self.setLayout(self.mainLayout)
 
         self.timer = QtCore.QTimer(self)
-        self.timer.setInterval(1) # in milliseconds
+        self.timer.setInterval(0) # in milliseconds
         self.timer.start()
         self.timer.timeout.connect(self.onNewData)
 
@@ -66,6 +66,9 @@ class MyWidget(pg.GraphicsWindow):
         self.plotItem.getViewBox().setRange(xRange=(-3000,6000),yRange=(-6000,4000))
 
         self.plotDataItem = self.plotItem.plot([], pen=None,symbolBrush=(255,0,0), symbolSize=5, symbolPen=None)
+
+
+
 
 
     def setData(self, x, y):
@@ -101,14 +104,207 @@ class MyWidget(pg.GraphicsWindow):
         n_noise_ = list(labels).count(-1)
         label_list = set(labels)
 
+        def getFeatures(clust_x, clust_y, clustersize):
+            x_mean = sum(clust_x)/clustersize
+            y_mean = sum(clust_y)/clustersize
+
+            clust_x_sorted = np.sort(clust_x)
+            clust_y_sorted = np.sort(clust_y)
+            x_median = np.median(clust_x_sorted)
+            y_median = np.median(clust_y_sorted)
+
+            distance = math.sqrt(x_median*x_median + y_median*y_median)
+
+            sum_std_diff = sum_med_diff = 0
+            for i in range(clustersize):
+                sum_std_diff += pow(clust_x[i]-x_mean, 2) + pow(clust_y[i]-y_mean, 2)
+                sum_med_diff += math.sqrt(pow(clust_x[i]-x_median, 2)+pow(clust_y[i] - y_median, 2))
+
+            std = math.sqrt(1/(clustersize-1)*sum_std_diff)
+            avg_med_dev = sum_med_diff / clustersize
+
+            first_elem = [clust_x[0],clust_y[0]]  #81
+            last_elem  = [clust_x[-1],clust_y[-1]]  #82
+
+            prev_ind = 0
+            next_ind = 0
+            prev_jump = 0
+            next_jump = 0
+            occluded_left = 0
+            occluded_right = 0
+
+            #122
+
+            width = math.sqrt(pow(clust_x[0]-clust_x[-1], 2) + pow(clust_y[0]-clust_y[-1],2))  #125 - Width
+
+            points=np.array((clustersize,2))
+
+            points2=np.vstack((clust_x,clust_y))
+            print('ff',points2.shape)
+            points2=np.transpose(points2)
+
+            print('ff',points2.shape)
+
+            W = np.zeros((2,2), np.float64)
+            w = np.zeros((2,2), np.float64)
+            U = np.zeros((clustersize, 2), np.float64)
+
+            V = np.zeros((2,2), np.float64)
+
+            w,u,vt=cv2.SVDecomp(points2,W,U,V)
+            print('ww',w,W)
+            rot_points = np.zeros((clustersize,2), np.float64)
+
+            W[0,0]=w[0]
+            W[1,1]=w[1]
+            rot_points = np.matmul(u,W)
+            # print(w)
+            # print(u)
+            # print(vt)
+            # print(rot_points.shape)
+
+            linearity=0
+            for i in range(clustersize):
+                linearity += pow(rot_points[i, 1], 2)
 
 
+            #Circularity
+            A = np.zeros((clustersize,3), np.float64)
+            B = np.zeros((clustersize,1), np.float64)
+
+
+            for i in range(clustersize):
+                A[i,0]=-2.0 * clust_x[i]
+                A[i,1]=-2.0 * clust_y[i]
+                A[i,2]=1
+                B[i,0]=math.pow(clust_x[i], 2)-math.pow(clust_y[i], 2)
+
+            sol = np.zeros((3,1),np.float64)
+            cv2.solve(A, B, sol, cv2.DECOMP_SVD)
+
+            xc = sol[0,0]
+            yc = sol[1,0]
+            rc = math.sqrt(pow(xc, 2)+pow(yc, 2)) - sol[2,0]
+
+
+            circularity = 0
+            for i in range(clustersize):
+                circularity += pow(rc - math.sqrt(pow(xc - clust_x[i], 2) + pow(yc-clust_y[i], 2)), 2)
+
+
+            radius = rc #Radius
+
+            mean_curvature = 0  #207 Mean_Curvature
+
+            boundary_length = 0  #Boundary_Length
+            last_boundary_seg = 0 #Boundary_Length
+            boundary_regularity = 0
+            sum_boundary_reg_sq = 0
+
+            #Mean Angualar Difference
+
+            left = 2
+            mid = 1
+            right=0
+
+            ang_diff=0
+
+            while(left!=clustersize):
+                mlx  =  clust_x[left] - clust_x[mid]
+                mly  =  clust_y[left] - clust_y[mid]
+                L_ml =  math.sqrt(mlx*mlx + mly*mly)
+
+                mrx  = clust_x[right] - clust_x[mid]
+                mry  = clust_y[right] - clust_y[mid]
+                L_mr = math.sqrt(mrx * mrx + mry * mry)
+
+                lrx  = clust_x[left] - clust_x[right]
+                lry  = clust_y[left] - clust_y[right]
+                L_lr = math.sqrt(lrx * lrx + lry * lry)
+
+
+                boundary_length+= L_mr
+                sum_boundary_reg_sq += L_mr*L_mr
+                last_boundary_seg = L_ml
+
+                A = (mlx * mrx + mly * mry) / pow(L_mr, 2)
+                B = (mlx * mry - mly * mrx) / pow(L_mr, 2)
+
+                th = math.atan2(B,A)
+
+                if th<0:
+                    th += 2*math.pi
+
+                ang_diff += th/clustersize
+
+                s = 0.5 * (L_ml + L_mr + L_lr)
+                area = math.sqrt(s * (s - L_ml) * (s - L_mr) * (s - L_lr))
+
+
+                if th>0:
+                    mean_curvature += 4 * (area) / (L_ml * L_mr * L_lr * clustersize)
+                else:
+                    mean_curvature -= 4 * (area) / (L_ml * L_mr * L_lr * clustersize)
+
+
+                left=left+1
+                mid=mid+1
+                right=right+1  #While loop ends
+
+
+            boundary_length += last_boundary_seg
+            sum_boundary_reg_sq += last_boundary_seg*last_boundary_seg
+
+
+
+            boundary_regularity = math.sqrt((sum_boundary_reg_sq - math.pow(boundary_length, 2) / clustersize)/(clustersize - 1))
+
+
+
+            #Mean Angular difference
+            first = 0
+            mid   = 1
+            last  = -1
+
+            sum_iav = 0
+            sum_iav_sq = 0
+
+            while(mid != clustersize):
+                mlx = clust_x[first] -clust_x[mid]
+                mly = clust_y[first] -clust_y[mid]
+
+                mrx  = clust_x[last]-clust_x[mid]
+                mry  = clust_y[last]-clust_y[mid]
+                L_mr = math.sqrt(mrx * mrx + mry * mry)
+
+                A = (mlx * mrx + mly * mry) / pow(L_mr, 2)
+                B = (mlx * mry - mly * mrx) / pow(L_mr, 2)
+
+                th = math.atan2(B, A)
+
+                if(th<0):
+                    th += 2 * math.pi
+
+                sum_iav += th
+                sum_iav_sq += th*th
+
+                mid = mid+1
+
+            iav = sum_iav/clustersize
+            std_iav = math.sqrt((sum_iav_sq - pow(sum_iav, 2) / clustersize) / (clustersize - 1))
+
+
+            features=[clustersize, std, avg_med_dev, width, linearity, circularity,
+                                radius, boundary_regularity, mean_curvature, ang_diff, iav, std_iav,
+                                distance, distance/clustersize]
+            return features
 
         for label in label_list:
             index = labels == label
             cluster = scan[index]
             # print(cluster.shape)
-            c1 = self.plotItem.plot(cluster[:, 0], cluster[:, 1], symbol='o', symbolPen=colors[label], name='red')
+            c1 = self.plotItem.plot(cluster[:, 0], cluster[:, 1], symbol='o', symbolPen=colors[label], name='red', symbolSize=5)
+            features = getFeatures(cluster[:, 0], cluster[:, 1], cluster.shape[0])
 
         # self.setData(scan[:,0], scan[:,1])
 
--- test2.py
+++ test2.py
@@ -1,39 +1,5 @@
+list = [2,3,4,5,6,7,8,9]
+print(list[0])
+print(list[0+2])
+print(len(list))
 
-
-from pyqtgraph.Qt import QtGui, QtCore
-import pyqtgraph as pg
-import numpy as np
-
-app = QtGui.QApplication([])
-mw = QtGui.QMainWindow()
-mw.resize(800,800)
-view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
-mw.setCentralWidget(view)
-mw.show()
-mw.setWindowTitle('pyqtgraph example: ScatterPlot')
-
-
-w1 = view.addPlot()  #p6=w1
-
-
-n = 2
-s1 = pg.ScatterPlotItem(size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 255, 255, 120))
-pos = np.random.normal(size=(2,n), scale=1e-5)
-dos = np.random.normal(size=(4,n), scale=1e-5)
-
-spots = [{'pos': pos[:,i], 'data': 1} for i in range(n)] + [{'pos': [0,0], 'data': 1}]
-dots = [{'pos': dos[:,i], 'data': 1} for i in range(2)] + [{'pos': [0,0], 'data': 1}]
-
-s1.addPoints(spots)
-s1.clear()
-s1.setData(dots)
-
-w1.addItem(s1)
-
-
-
-
-if __name__ == '__main__':
-    import sys
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
